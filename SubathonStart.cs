11500f5c8c374d49580585272a26e0c39539574d
rondhi <ronald@distor.co>
2022-12-21 00:17:46 -0800

    Update SubathonStart.cs
    - Add automatic backup/pause/resume support by saving to a text file. You'll have to point to a text file path in the configuration action.
    - Somehow lost some math to add in remaining time when limit has been reached brought it back in from older code
    - General bug fixes and code optimization

--- SubathonStart.cs
+++ SubathonStart.cs
@@ -2,13 +2,20 @@
 //Add System.dll as a reference
 using System;
 using System.Timers;
+using System.IO;
 
 class CPHInline
 {
     public System.Timers.Timer countdownTimer;
-    public int subathonSecondsLeft;
-    public int subathontotalTimeInSeconds;
+    public int subathonSecondsLeft;     //time left on timer
+    public int subathonTotalTimeInSeconds;  //total elapsed time after adding time
     public int subathonCapInSeconds;
+	public string subathonSecondsLeftFile;
+	public string subathonTotalTimeInSecondsFile;
+    public bool limitReached;
+    public bool messageOnce;
+
+
     public void Init()
     {
         countdownTimer = new System.Timers.Timer(1000);
@@ -20,11 +27,18 @@ class CPHInline
 
     public bool StartSubathon()
     {
-        double maxHourValue = Convert.ToDouble(args["maxHourValue"]);
-        double hourValue = Convert.ToDouble(args["hourValue"]);
-        subathonCapInSeconds = (int)(maxHourValue * 3600);
-        subathonSecondsLeft = (int)(hourValue * 3600 + 1);
-        subathontotalTimeInSeconds = subathonSecondsLeft;
+		limitReached = false;
+		messageOnce = false;
+		double maxHourValue = Convert.ToDouble(CPH.GetGlobalVar<double>("maxHourValueGlobal"));
+        double hourValue = Convert.ToDouble(args["hourValue"]); // Import arguments from UI
+
+        TimeSpan maxDuration = TimeSpan.FromHours(maxHourValue);
+        subathonCapInSeconds = (int)maxDuration.TotalSeconds; // Calculate the total length of the subathon in seconds
+
+        TimeSpan remainingDuration = TimeSpan.FromHours(hourValue);
+        subathonSecondsLeft = (int)remainingDuration.TotalSeconds; // Calculate the time remaining in seconds
+
+        subathonTotalTimeInSeconds = subathonSecondsLeft;
         countdownTimer.Start();
         return true;
     }
@@ -32,10 +46,29 @@ class CPHInline
     public void OnTimedEvent(Object source, ElapsedEventArgs e)
     {
         subathonSecondsLeft--;
+        if (subathonSecondsLeft % 300 == 0)
+        {
+            Backup();
+        }
+
         TimeSpan time = TimeSpan.FromSeconds(subathonSecondsLeft);
-        string countdownString = time.ToString(@"d' days 'hh\:mm\:ss");
+        string countdownString = "";
+        if (time.Days > 1)
+        {
+            countdownString = time.ToString(@"dd' days 'hh\:mm\:ss");
+        }
+        else if (time.Days == 1)
+        {
+            countdownString = time.ToString(@"dd' day 'hh\:mm\:ss");
+        }
+        else
+        {
+            countdownString = time.ToString(@"hh\:mm\:ss");
+        }
+
         if (subathonSecondsLeft == 0)
         {
+            File.WriteAllText(subathonSecondsLeftFile, "");
             StopSubathon("Subathon Complete!");
             CPH.RunAction("Subathon Done Action");
         }
@@ -52,6 +85,73 @@ class CPHInline
         countdownTimer.Dispose();
     }
 
+    public void Backup()
+    {
+        string secondsLeftBackupDirectory = CPH.GetGlobalVar<string>("secondsLeftBackupDirectory", true);
+        subathonSecondsLeftFile = (secondsLeftBackupDirectory + "subathonSecondsLeft.txt").ToString();
+        subathonTotalTimeInSecondsFile = (secondsLeftBackupDirectory + "subathonTotalTimeInSeconds.txt").ToString();
+        File.WriteAllText(subathonSecondsLeftFile, subathonSecondsLeft.ToString());
+        File.WriteAllText(subathonTotalTimeInSecondsFile, subathonTotalTimeInSeconds.ToString());
+    }
+
+    public bool PauseSubathon()
+    {
+        countdownTimer.Stop();
+        Backup();
+        TimeSpan time = TimeSpan.FromSeconds(subathonSecondsLeft);
+        string countdownString = "";
+        if (time.Days > 1)
+        {
+            countdownString = time.ToString(@"dd' days 'hh\:mm\:ss");
+        }
+        else if (time.Days == 1)
+        {
+            countdownString = time.ToString(@"dd' day 'hh\:mm\:ss");
+        }
+        else
+        {
+            countdownString = time.ToString(@"hh\:mm\:ss");
+        }
+
+        CPH.SendMessage($"Pausing subathon timer with {countdownString} left", true);
+        StopSubathon("Subathon paused...");
+        return true;
+    }
+
+    public bool ResumeSubathon()
+    {
+		double maxHourValue = Convert.ToDouble(CPH.GetGlobalVar<double>("maxHourValueGlobal"));
+        TimeSpan maxDuration = TimeSpan.FromHours(maxHourValue);
+        subathonCapInSeconds = (int)maxDuration.TotalSeconds; // Calculate the total length of the subathon in seconds
+        if (!string.IsNullOrEmpty(subathonSecondsLeftFile))
+        {
+            subathonSecondsLeft = Int32.Parse(subathonSecondsLeftFile) + 1; // Resuming seconds left from backup
+            subathonTotalTimeInSeconds = Int32.Parse(subathonTotalTimeInSecondsFile); //Recalling total time elapsed
+            TimeSpan time = TimeSpan.FromSeconds(subathonSecondsLeft -1);
+            string countdownString = "";
+            if (time.Days > 1)
+            {
+                countdownString = time.ToString(@"dd' days 'hh\:mm\:ss");
+            }
+            else if (time.Days == 1)
+            {
+                countdownString = time.ToString(@"dd' day 'hh\:mm\:ss");
+            }
+            else
+            {
+                countdownString = time.ToString(@"hh\:mm\:ss");
+            }
+            CPH.SendMessage($"Resuming subathon timer with {countdownString} left", true);
+			countdownTimer.Start();
+        }
+        else
+        {
+            CPH.SendMessage("Cannot resume subathon because there is no evidence of a previous subathon.", true);
+        }
+
+        return true;
+    }
+
     private void StopSubathon(string message)
     {
         // Set to Scene and Source of your text source
@@ -64,53 +164,69 @@ class CPHInline
     private void AddTime(int timeToAdd)
     {
         int secondsToAdd = Convert.ToInt32(timeToAdd);
-        if ((subathontotalTimeInSeconds + secondsToAdd) < subathonCapInSeconds)
+        if ((subathonCapInSeconds - (subathonTotalTimeInSeconds + secondsToAdd)) > 0 )
         {
-            subathontotalTimeInSeconds = subathontotalTimeInSeconds + secondsToAdd;
+            subathonTotalTimeInSeconds = subathonTotalTimeInSeconds + secondsToAdd;
             subathonSecondsLeft = subathonSecondsLeft + secondsToAdd;
-            if (secondsToAdd < 60)
-            {
-                string message = secondsToAdd + " seconds has been added to the Subathon Timer";
-                CPH.SendMessage(message, true);
-            }
-            else
-            {
-                double secondsDouble = Convert.ToDouble(secondsToAdd);
-                int timeMinutes = Convert.ToInt32(Math.Floor(secondsDouble / 60));
-                int timeSeconds = Convert.ToInt32(Math.Floor(secondsDouble % 60));
-                if (timeMinutes == 1 && timeSeconds == 0)
-                {
-                    string message = timeMinutes + " minute has been added to the Subathon Timer";
-                    CPH.SendMessage(message, true);
-                }
-                else if (timeMinutes > 1 && timeSeconds == 0)
-                {
-                    string message = timeMinutes + " minutes has been added to the Subathon Timer";
-                    CPH.SendMessage(message, true);
-                }
-                else if (timeMinutes == 1 && timeSeconds > 0)
-                {
-                    string message = timeMinutes + " minute and " + timeSeconds + " seconds has been added to the Subathon Timer";
-                    CPH.SendMessage(message, true);
-                }
-                else if (timeMinutes > 1 && timeSeconds > 0)
-                {
-                    string message = timeMinutes + " minutes and " + timeSeconds + " seconds has been added to the Subathon Timer";
-                    CPH.SendMessage(message, true);
-                }
-            }
         }
         else
         {
-            subathonSecondsLeft = subathonSecondsLeft + (subathonCapInSeconds - subathontotalTimeInSeconds);
-            subathontotalTimeInSeconds = subathonCapInSeconds;
-            CPH.SendMessage("We've reached the sub-a-thon limit! No more time will be added.", true);
+            subathonSecondsLeft = subathonSecondsLeft + (subathonCapInSeconds - subathonTotalTimeInSeconds);
+			secondsToAdd = subathonCapInSeconds - subathonTotalTimeInSeconds;
+            subathonTotalTimeInSeconds = subathonCapInSeconds;
+			limitReached = true;
         }
+        // Calculate the number of hours, minutes, and seconds added
+            double secondsDouble = Convert.ToDouble(secondsToAdd);
+            int timeHours = Convert.ToInt32(Math.Floor(secondsDouble / 3600));
+            int timeMinutes = Convert.ToInt32(Math.Floor((secondsDouble % 3600) / 60));
+            int timeSeconds = Convert.ToInt32(Math.Floor(secondsDouble % 60));
+            // Build the message string
+            string message = "";
+            if (timeHours > 0)
+            {
+                message += $"{timeHours} hour{(timeHours > 1 ? "s" : "")}";
+            }
+
+            if (timeMinutes > 0)
+            {
+                if (!string.IsNullOrEmpty(message))
+                {
+                    message += " ";
+                }
+
+                message += $"{timeMinutes} minute{(timeMinutes > 1 ? "s" : "")}";
+            }
+
+            if (timeSeconds > 0)
+            {
+                if (!string.IsNullOrEmpty(message))
+                {
+                    message += " ";
+                }
+
+                message += $"{timeSeconds} second{(timeSeconds > 1 ? "s" : "")}";
+            }
+
+            if (secondsToAdd == 0)
+            {
+                message += "No time";
+            }
+
+            message += " has been added to the subathon countdown timer";
+            CPH.SendMessage(message, true);
+			if (limitReached == true && messageOnce == false)
+            {
+                CPH.SendMessage("Congratulations! The subathon limit has been reached!",true);
+                messageOnce = true;
+            }
     }
 
     public bool Stop()
     {
-        StopSubathon("Sub-a-thon cancelled!");
+        StopSubathon("Subathon cancelled!");
+        string secondsLeftBackupDirectory = CPH.GetGlobalVar<string>("secondsLeftBackupDirectory", true);
+        File.WriteAllText(secondsLeftBackupDirectory, "");
         return true;
     }
 
